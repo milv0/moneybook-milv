9582efaf5c032b9a0c128cb2a7da46ee220397e2
quantbrainz <quantbrainz@gmail.com>
2023-07-24 17:07:58 +0900

    d

--- app/backend/app.py
+++ app/backend/app.py
@@ -1,10 +1,10 @@
 import streamlit as st
 import pandas as pd
 
-# Create an empty dataframe to store expenses
+
 expenses = pd.DataFrame(columns=['Date', 'Category', 'Amount'])
 
-# Sidebar - Add expense
+
 st.sidebar.header("입력란")
 
 date = st.sidebar.date_input("Date", value=pd.Timestamp.now())
@@ -16,7 +16,7 @@ if st.sidebar.button("Add Expense"):
     expenses = pd.concat([expenses, new_expense], ignore_index=True)
     st.sidebar.success("Expense added successfully!")
 
-# Main content
+
 st.title("가계부")
 
 st.header("내역")
@@ -26,11 +26,9 @@ st.header("Summary")
 total_expenses = expenses['Amount'].sum()
 st.write("Total Expenses:", total_expenses)
 
-# Visualize expenses by category
 expenses_by_category = expenses.groupby('Category')['Amount'].sum()
 st.bar_chart(expenses_by_category)
 
-# Save expenses to Excel
 if st.button("Save to Excel"):
     file_name = "moneybook.csv"
     expenses.to_excel(file_name, index=False)
